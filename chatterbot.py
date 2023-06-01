e7ae00d8ff77da6ffbf8ec5cb3fac21aed438193
bmi17 <106488165+bmi17@users.noreply.github.com>
2023-06-02 03:12:46 +0530

    Create chatterbot.py

---
+++ chatterbot.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+chatbot = ChatBot('MyChatBot')
+trainer = ListTrainer(chatbot)
+training_data = [
+    ["Hi there!", "Hello! How can I assist you today?"],
+    ["What services do you offer?", "We offer a wide range of services to cater to different needs."],
+    ["How can I contact customer support?", "You can reach our customer support team at support@example.com."],
+    # Add more conversation pairs here
+]
+trainer.train(training_data)
+response = chatbot.get_response('Hello')
+print(response)
+
+
+
+#sanu's old code
