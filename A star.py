e708dd345647d3966985bec593cd50533a461570
bmi17 <106488165+bmi17@users.noreply.github.com>
2023-06-02 04:36:27 +0530

    Update A star.py

--- A star.py
+++ A star.py
@@ -145,105 +145,192 @@ main()
 
 
 # 2. second code ( for myself)
-# A* algorithm implemented for the sliding puzzle game:
+g = 0  # Global variable to keep track of the number of moves
 
-# The sliding puzzle game is a classic puzzle game where the player must rearrange numbered 
-# tiles within a grid by sliding them into the empty space until they are in a specific predefined order.
-
-# The astar_sliding_puzzle function is the main function that implements the A* algorithm for the sliding puzzle game.
-# It takes the start_state and target_state as inputs.
-
-# The moves variable defines the possible moves in the puzzle, which include moving tiles to the right, down, left, and up.
-
-# The get_heuristic function calculates the heuristic value for a given state. In the sliding puzzle game, the heuristic is
-# calculated as the sum of the Manhattan distances between each tile and its target position (ignoring the blank tile).
-
-# The generate_next_states function generates the possible next states given a current state. It checks the empty cell's 
-# neighboring positions and swaps the empty cell with a neighboring tile to create a new state. It returns a list of all the possible next states.
-
-# The get_empty_cell function finds the position of the empty cell (represented by the value 0) in a given state.
-
-# The open_set is a priority queue that stores the states to be explored. Each state is a tuple containing the heuristic value
-# and the state itself. It is initially populated with the start state.
-
-# The g_scores dictionary stores the cost to reach each state from the start state.
-
-# The A* algorithm's main loop starts with a priority queue that retrieves the state with the lowest heuristic value (priority) 
-# from the open_set.
-
-# If the current state is equal to the target state, a solution has been found, and the function returns True.
-
-# If the current state is not the target state, the algorithm generates the possible next states using the generate_next_states
-# function.
-
-# For each next state, the g_score is calculated as the cost to reach the current state plus one.
-
-# If the g_score for the next state is lower than the existing g_score (or infinity if no g_score exists), the g_scores 
-# dictionary is updated, and the next state is added to the open_set with its priority calculated as the sum of g_score and the heuristic value.
-
-# If the open_set becomes empty, it means no solution is found, and the function returns False.
-
-# After the main loop, the astar_sliding_puzzle function is called with the start and target states, and the return value
-# is stored in the is_solvable variable.
-
-# Finally, based on the value of is_solvable, the program prints whether the puzzle is solvable 
-# or not.
+# Function to print the puzzle board
+def print_board(elements):
+    for i in range(9):
+        if i % 3 == 0:
+            print()  # Start a new line for every 3 elements
+        if elements[i] == -1:
+            print("_", end=" ")  # Print "_" for the empty space
+        else:
+            print(elements[i], end=" ")  # Print the number for non-empty spaces
+    print()  # Start a new line after printing the board
 
 
-import heapq
+# Function to check if the puzzle is solvable
+def solvable(start):
+    inv = 0
 
-class NumberPuzzle:
-    def __init__(self, start_state, target_state):
-        self.start_state = start_state
-        self.target_state = target_state
-        self.moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for i in range(9):
+        if start[i] <= 1:
+            continue
+        for j in range(i + 1, 9):
+            if start[j] == -1:
+                continue
+            if start[i] > start[j]:
+                inv += 1
+    if inv % 2 == 0:
+        return True  # Solvable if the number of inversions is even
+    return False
 
-    def get_heuristic(self, state):
-        # Calculate the heuristic value as the number of misplaced tiles
-        return sum(state[i][j] != self.target_state[i][j] for i in range(len(state)) for j in range(len(state[0])))
 
-    def generate_next_states(self, state):
-        # Generate the possible next states by swapping the empty cell with its neighboring tiles
-        next_states = []
-        empty_row, empty_col = next((i, j) for i, row in enumerate(state) for j, val in enumerate(row) if val == 0)
-        for move in self.moves:
-            new_row, new_col = empty_row + move[0], empty_col + move[1]
-            if 0 <= new_row < len(state) and 0 <= new_col < len(state[0]):
-                # Swap the empty cell with a neighboring tile
-                new_state = [row.copy() for row in state]
-                new_state[empty_row][empty_col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[empty_row][empty_col]
-                next_states.append(new_state)
-        return next_states
+# Function to calculate the heuristic value for a state
+def heuristic(start, goal):
+    global g
+    h = 0
+    for i in range(9):
+        for j in range(9):
+            if start[i] == goal[j] and start[i] != -1:
+                h += (abs(j - i)) // 3 + (abs(j - i)) % 3
+    return h + g
 
-    def solve(self):
-        open_set = [(self.get_heuristic(self.start_state), self.start_state)]
-        g_scores = {tuple(map(tuple, self.start_state)): 0}
-        while open_set:
-            _, current_state = heapq.heappop(open_set)
-            if current_state == self.target_state:
-                # Target state is found, puzzle is solvable
-                return True
-            for next_state in self.generate_next_states(current_state):
-                g_score = g_scores[tuple(map(tuple, current_state))] + 1
-                if g_score < g_scores.get(tuple(map(tuple, next_state)), float('inf')):
-                    # Update g_score if a better path is found
-                    g_scores[tuple(map(tuple, next_state))] = g_score
-                    heapq.heappush(open_set, (g_score + self.get_heuristic(next_state), next_state))
-        # No solution found, puzzle is not solvable
-        return False
 
-# Define the start and target states
-start_state = [[2, 8, 3], [1, 6, 4], [7, 0, 5]]
-target_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+# Functions to move the empty space in different directions
+def moveleft(start, position):
+    start[position], start[position - 1] = start[position - 1], start[position]
 
-# Create a NumberPuzzle instance
-puzzle = NumberPuzzle(start_state, target_state)
 
-# Solve the puzzle
-is_solvable = puzzle.solve()
+def moveright(start, position):
+    start[position], start[position + 1] = start[position + 1], start[position]
 
-# Print the result
-if is_solvable:
-    print("The puzzle is solvable!")
-else:
-    print("No solution found.")
+
+def moveup(start, position):
+    start[position], start[position - 3] = start[position - 3], start[position]
+
+
+def movedown(start, position):
+    start[position], start[position + 3] = start[position + 3], start[position]
+
+
+# Function to determine the best move for the empty space
+def movetile(start, goal):
+    emptyat = start.index(-1)
+    row = emptyat // 3
+    col = emptyat % 3
+    t1, t2, t3, t4 = start[:], start[:], start[:], start[:]
+    f1, f2, f3, f4 = 100, 100, 100, 100
+
+    if col - 1 >= 0:
+        moveleft(t1, emptyat)
+        f1 = heuristic(t1, goal)
+    if col + 1 < 3:
+        moveright(t2, emptyat)
+        f2 = heuristic(t2, goal)
+    if row + 1 < 3:
+        movedown(t3, emptyat)
+        f3 = heuristic(t3, goal)
+    if row - 1 >= 0:
+        moveup(t4, emptyat)
+        f4 = heuristic(t4, goal)
+
+    min_heuristic = min(f1, f2, f3, f4)
+
+    if f1 == min_heuristic:
+        moveleft(start, emptyat)
+    elif f2 == min_heuristic:
+        moveright(start, emptyat)
+    elif f3 == min_heuristic:
+        movedown(start, emptyat)
+    elif f4 == min_heuristic:
+        moveup(start, emptyat)
+
+
+# Function to solve the 8-puzzle using A* search algorithm
+def solveEight(start, goal):
+    global g
+    g += 1
+    movetile(start, goal)
+    print_board(start)  # Print the current state of the puzzle board
+    f = heuristic(start, goal)
+    if f == g:
+        print("Solved in {} moves".format(f))
+        return
+
+    solveEight(start, goal)  # Recursively call the function to continue solving
+
+
+# Main function
+def main():
+    global g
+    start = list()
+    goal = list()
+    print("Enter the start state:(Enter -1 for empty):")
+    for i in range(9):
+        start.append(int(input()))  # Read the start state input from the user
+
+    print("Enter the goal state:(Enter -1 for empty):")
+    for i in range(9):
+        goal.append(int(input()))  # Read the goal state input from the user
+
+    print_board(start)  # Print the initial state of the puzzle board
+
+    # Check if the puzzle is solvable
+    if solvable(start):
+        solveEight(start, goal)  # Solve the puzzle
+        print("Solved in {} moves".format(g))
+    else:
+        print("Not possible to solve")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+# Test Cases
+# 
+# 1
+# 2
+# 3
+# -1
+# 4 
+# 6
+# 7 
+# 5 
+# 8 
+
+# 1 
+# 2 
+# 3 
+# 4 
+# 5 
+# 6 
+# 7 
+# 8
+# -1
+
+
+
+
+# # The code you provided appears to be an implementation of the A* search algorithm for solving the 8-puzzle problem. 
+# The 8-puzzle is a sliding puzzle that consists of a 3x3 grid with 8 numbered tiles and one empty space. The goal is to rearrange the tiles from a given start state to a desired goal state by sliding them into the empty space.
+
+# # The code includes several functions:
+
+# # 1. `print_board(elements)`: This function is used to print the current state of the puzzle board in a human-readable format.
+
+# # 2. `solvable(start)`: This function checks whether a given start state of the puzzle is solvable or not.
+# It uses the concept of inversion count to determine solvability. If the number of inversions (the number of tiles that are out of their natural order) is even, the puzzle is solvable.
+
+# # 3. `heuristic(start, goal)`: This function calculates the heuristic value (h) for a given state by counting 
+# the number of misplaced tiles in the current state compared to the goal state. It uses the Manhattan distance heuristic, which calculates the minimum number of moves required for each tile to reach its goal position.
+
+# # 4. `moveleft(start, position)`, `moveright(start, position)`, `moveup(start, position)`, `movedown(start, position)
+# `: These functions are used to move the empty space (represented by -1) in the puzzle board left, right, up, or down, respectively.
+
+# # 5. `movetile(start, goal)`: This function determines the best move for the empty space by calculating the heuristic 
+# values for each possible move. It selects the move that minimizes the heuristic value.
+
+# # 6. `solveEight(start, goal)`: This function implements the A* search algorithm to solve the puzzle. It repeatedly
+# calls the `movetile` function to make moves until the puzzle reaches the goal state. It also keeps track of the number of moves (g) made to reach the current state.
+
+# # 7. `main()`: This is the main function that takes user input for the start and goal states of the puzzle and calls the
+# necessary functions to solve it.
+
+# # To use the code, you can enter the start and goal states of the 8-puzzle as input. The start and goal states should be 
+# entered as a sequence of 9 numbers, where -1 represents the empty space. After entering the input, the code will attempt to solve the puzzle and display the steps taken to reach the goal state.
+
+# # Note that the code does not include any error handling or input validation, so it assumes correct and valid input from 
+# the user.
