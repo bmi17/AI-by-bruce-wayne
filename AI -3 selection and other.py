118680c222693f341fd25c3ab61060547c356777
bmi17 <106488165+bmi17@users.noreply.github.com>
2023-06-02 03:52:49 +0530

    Update AI -3 selection and other.py

--- AI -3 selection and other.py
+++ AI -3 selection and other.py
@@ -1,3 +1,12 @@
+# Implement Greedy search algorithm for any of the following application:
+# I. Selection Sort
+# II. Minimum Spanning Tree
+# III. Single-Source Shortest Path Problem
+# IV. Job Scheduling Problem
+# V. Prim's Minimal Spanning Tree Algorithm
+# VI. Kruskal's Minimal Spanning Tree Algorithm
+# VII. Dijkstra's Minimal Spanning Tree Algorithm
+
 
 def selectionSort(A):
     U = A.copy()
